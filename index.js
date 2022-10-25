908a4d293f0ca7c3b0de0ce597489e05e302997a
Felix-fale <falefelix23@gmail.com>
2022-10-25 23:34:01 +0000

    last commit

---
+++ index.js
@@ -0,0 +1,11 @@
+btn.onclick = () => {
+  fetch("https://api.github.com/users/" + champ.value) // maniere d'appeler un id (a eviter)
+    .then((reponse) => reponse.json())
+    .then((data) => {
+      output.textContent = "";
+      output.textContent = `Compte de ${data.name}`;
+      const img = document.createElement("img");
+      img.src = data.avatar_url;
+      output.appendChild(img);
+    });
+};
